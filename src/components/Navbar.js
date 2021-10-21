0e8cadbfcec5075546ec2800bab7b866968aa07f
monika12b <monika12b@gmail.com>
2021-10-21 12:27:58 -0700

    sticky top nav fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,58 +25,56 @@ class NavigationBar extends Component {
   render() {
     return (
       <React.Fragment>
-        <section className='nav-section'>
-          <div className='logo-container'>
-            <img src={logo} alt='planets' className='logo' />
-          </div>
+        <div className='logo-container'>
+          <img src={logo} alt='planets' className='logo' />
+        </div>
 
-          <Navbar sticky='top' expand='md' className='navBar'>
-            <NavbarToggler onClick={this.toggleNav} />
-            <Collapse isOpen={this.state.isNavOpen} navbar>
-              <Nav navbar className='m-auto'>
-                {/* <NavItem>
+        <Navbar sticky='top' expand='md' className='navBar'>
+          <NavbarToggler onClick={this.toggleNav} />
+          <Collapse isOpen={this.state.isNavOpen} navbar>
+            <Nav navbar className='m-auto'>
+              {/* <NavItem>
                 <NavLink>
                   <img src={logo} alt='planets' className='logo inline-block' />
                 </NavLink>
               </NavItem> */}
-                <div className='nav-btns d-flex'>
-                  <NavItem>
-                    <NavLink className='nav_link' to='/story'>
-                      Story
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink className='nav_link gallery_btn' to='/story'>
-                      Gallery
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink className='nav_link' to='/roadmap'>
-                      Roadmap
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink className='nav_link' to='/game-mechanics'>
-                      Game Mechanics
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink className='nav_link' to='/faq'>
-                      FAQ
-                    </NavLink>
-                  </NavItem>
-                </div>
-                <div className='nav-wallet'>
-                  <NavItem>
-                    <NavLink className='nav_link wallet' to='/wallet'>
-                      Connect Wallet <i class='fas fa-wallet'></i>
-                    </NavLink>
-                  </NavItem>
-                </div>
-              </Nav>
-            </Collapse>
-          </Navbar>
-        </section>
+              <div className='nav-btns d-flex'>
+                <NavItem>
+                  <NavLink className='nav_link' to='/story'>
+                    Story
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className='nav_link gallery_btn' to='/story'>
+                    Gallery
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className='nav_link' to='/roadmap'>
+                    Roadmap
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className='nav_link' to='/game-mechanics'>
+                    Game Mechanics
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className='nav_link' to='/faq'>
+                    FAQ
+                  </NavLink>
+                </NavItem>
+              </div>
+              <div className='nav-wallet'>
+                <NavItem>
+                  <NavLink className='nav_link wallet' to='/wallet'>
+                    Connect Wallet <i class='fas fa-wallet'></i>
+                  </NavLink>
+                </NavItem>
+              </div>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </React.Fragment>
     );
   }
