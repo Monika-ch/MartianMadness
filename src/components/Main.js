32cee77f9e8dc9f9c0b64c2797ab009fd7683003
monika12b <monika12b@gmail.com>
2021-12-13 19:10:45 -0800

    attmpt to fix build break

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,39 +1,40 @@
 import React, { Component } from "react";
-import HomePage from "./HomePage";
-import Story from "./Story";
-import Gallery from "./Gallery";
-import Roadmap from "./Roadmap";
-import GameMechanics from "./GameMechanics";
-import Teams from "./Teams";
-import Faq from "./Faq";
-import Footer from "./Footer";
+// import HomePage from "./HomePage";
+// import Story from "./Story";
+// import Gallery from "./Gallery";
+// import Roadmap from "./Roadmap";
+// import GameMechanics from "./GameMechanics";
+// import Teams from "./Teams";
+// import Faq from "./Faq";
+// import Footer from "./Footer";
 // import Wallet from "./Wallet";
 
 export default function Main() {
   return (
-    <div>
-      <section id='home'>
-        <HomePage />
-      </section>
-      <section id='story'>
-        <Story />
-      </section>
-      <section id='gallery'>
-        <Gallery />
-      </section>
-      <section id='roadmap'>
-        <Roadmap />
-      </section>
-      <section id='gameMechanics'>
-        <GameMechanics />
-      </section>
-      <section id='teams'>
-        <Teams />
-      </section>
-      <section id='faq'>
-        <Faq />
-      </section>
-      <Footer />
-    </div>
+    // <div>
+    //   <section id='home'>
+    //     <HomePage />
+    //   </section>
+    //   <section id='story'>
+    //     <Story />
+    //   </section>
+    //   <section id='gallery'>
+    //     <Gallery />
+    //   </section>
+    //   <section id='roadmap'>
+    //     <Roadmap />
+    //   </section>
+    //   <section id='gameMechanics'>
+    //     <GameMechanics />
+    //   </section>
+    //   <section id='teams'>
+    //     <Teams />
+    //   </section>
+    //   <section id='faq'>
+    //     <Faq />
+    //   </section>
+    //   <Footer />
+    // </div>
+    <div>Hello</div>
   );
 }
