44ca8c50b3335e2be83054fa9c1405e759c272ec
monika12b <monika12b@gmail.com>
2022-01-23 22:47:20 -0800

    homepage/navbar for mobile screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -84,43 +84,12 @@ class NavigationBar extends Component {
             <Collapse isOpen={this.state.isNavOpen} navbar>
               <Nav navbar className='m-auto'>
                 {/* <NavItem>
-                <NavLink>
-                  <img src={logo} alt='planets' className='logo inline-block' />
-                </NavLink>
-              </NavItem> */}
-                {/* <NavItem>
-                  <NavLink className='nav_link' to='/story'>
-                    Story
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className='nav_link gallery_btn' to='/gallery'>
-                    Gallery
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className='nav_link' to='/roadmap'>
-                    Roadmap
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className='nav_link' to='/game-mechanics'>
-                    Game Mechanics
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className='nav_link' to='/faq'>
-                    FAQ
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className='nav_link wallet' to='/wallet'>
-                    Connect Wallet <i class='fas fa-wallet'></i>
-                  </NavLink>
-                </NavItem>
-                <NavItem className='d-sm-block d-md-none'>
-                  <NavLink className='nav_link nav-mint-btn' to='/wallet'>
-                    Mint <i class='far fa-certificate'></i>
+                  <NavLink>
+                    <img
+                      src={`${process.env.PUBLIC_URL}/assets/images/logo.gif`}
+                      alt='Martian Madness Logo'
+                      className='logo'
+                    />{" "}
                   </NavLink>
                 </NavItem> */}
 
